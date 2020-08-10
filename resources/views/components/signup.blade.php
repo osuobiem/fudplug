fb38860fa0e3898f9bfc8476f792c9f005af426a
wisdomNtui <wisdomntui@gmail.com>
2020-08-10 21:38:43 +0100

    login and signup forms added

--- resources/views/components/signup.blade.php
+++ resources/views/components/signup.blade.php
@@ -1,59 +1,174 @@
-<div class="post-popup col-lg-6 col-md-8" id="sign-up-modal">
-  <div class="post-project" id="sign-up-inner">
-    <h3 class="curve-top">Sign Up</h3>
-    <div class="post-project-fields curve-bottom">
+<!--div class="post-popup col-lg-6 col-md-8" id="sign-up-modal">
+    <div class="post-project" id="sign-up-inner">
+        <h3 class="curve-top">Sign Up</h3>
+        <div class="post-project-fields curve-bottom">
 
 
-      <div class="form-group col-md-12">
-        <label for="who">Sign up as</label>
-        <select id="who" class="form-control mt-2">
-          <option selected disabled>Please Select</option>
-          <option value="vendor">Food Vendor</option>
-          <option value="user">User</option>
-        </select>
-      </div>
+            <div class="form-group col-md-12">
+                <label for="who">Sign up as</label>
+                <select id="who" class="form-control mt-2">
+                    <option selected disabled>Please Select</option>
+                    <option value="vendor">Food Vendor</option>
+                    <option value="user">User</option>
+                </select>
+            </div>
 
-      <hr>
+            <hr>
 
-      <form id="vendor-signup-form" class="row">
-        <div class="form-group col-md-12">
-          <label for="business_name">Business Name</label>
-          <input type="text" class="form-control mt-2" id="business_name" placeholder="Business Name">
+            <form id="vendor-signup-form" class="row">
+                <div class="form-group col-md-12">
+                    <label for="business_name">Business Name</label>
+                    <input type="text" class="form-control mt-2" id="business_name" placeholder="Business Name">
+                </div>
+
+                <div class="form-group col-md-12">
+                    <label for="username">Username</label>
+                    <input type="text" class="form-control mt-2" id="username" placeholder="Username">
+                </div>
+
+                <div class="form-group col-md-12">
+                    <label for="e<div class="form-group col-md-12">
+                <label for="who">Sign up as</label>
+                <select id="who" class="form-control mt-2">
+                    <option selected disabled>Please Select</option>
+                    <option value="vendor">Food Vendor</option>
+                    <option value="user">User</option>
+                </select>
+            </div>ext" class="form-control mt-2" id="phone" placeholder="Phone Number">
+                </div>
+
+                <div class="form-group col-md-12">
+                    <label for="password">Password</label>
+                    <input type="password" class="form-control mt-2" id="password" placeholder="Password">
+                </div>
+            </form>
+
+
+            <div class="form-group col-md-6 mx-auto mt-3">
+                <button class="btn form-btn">Sign Up</button>
+            </div>
         </div>
 
-        <div class="form-group col-md-12">
-          <label for="username">Username</label>
-          <input type="text" class="form-control mt-2" id="username" placeholder="Username">
+        <div class="col-md-12 mt-2 text-center">
+            <p>Already have an account?<br><a href="#" id="login-lk" class="fud-link">Login</a></p>
         </div>
 
-        <div class="form-group col-md-12">
-          <label for="email">Email</label>
-          <input type="email" class="form-control mt-2" id="email" placeholder="Email">
-        </div>
-
-        <div class="form-group col-md-12">
-          <label for="phone">Phone</label>
-          <input type="text" class="form-control mt-2" id="phone" placeholder="Phone Number">
-        </div>
-
-        <div class="form-group col-md-12">
-          <label for="password">Password</label>
-          <input type="password" class="form-control mt-2" id="password" placeholder="Password">
-        </div>
-      </form>
-
-
-      <div class="form-group col-md-6 mx-auto mt-3">
-        <button class="btn form-btn">Sign Up</button>
-      </div>
     </div>
 
-    <div class="col-md-12 mt-2 text-center">
-      <p>Already have an account?<br><a href="#" id="login-lk" class="fud-link">Login</a></p>
-    </div>
-
-  </div>
-
-  <a href="#" title=""><i class="la la-times-circle-o"></i></a>
+    <a href="#" title=""><i class="la la-times-circle-o"></i></a>
 </div>
+</div-->
+
+<div class="post-popup col-lg-6 col-md-8" id="sign-up-modal">
+    <div class="post-project" id="sign-up-inner">
+        <h3 class="curve-top">Sign Up</h3>
+        <div class="post-project-fields curve-bottom py-0">
+            <div class="main-ws-sec">
+
+                <div class="user-tab-sec rewivew">
+                    <h4 class="curve-top text-center mb-n1">as</h4>
+                    <div class="tab-feed st2 settingjb text-center border-bottom pb-2">
+                        <ul>
+                            <li data-tab="feed-dd" class="active col-md-4">
+                                <a href="#" title="">
+                                    <span>Vendor</span>
+                                </a>
+                            </li>
+                            <li data-tab="info-dd" class="col-md-4 border-left">
+                                <a href="#" title="">
+                                    <span>User</span>
+                                </a>
+                            </li>
+                        </ul>
+                    </div><!-- tab-signup end-->
+                </div>
+                <!--user-tab-sec end-->
+                <div class="product-feed-tab current pt-3" id="feed-dd" style="height: 300px; overflow-y: scroll;">
+                    <form id="vendor-signup-form" class="row my-0">
+                        <div class="form-group col-md-12">
+                            <label for="business_name">Business Name</label>
+                            <input type="text" class="form-control mt-2" id="business_name" placeholder="Business Name">
+                        </div>
+
+                        <div class="form-group col-md-12">
+                            <label for="username">Username</label>
+                            <input type="text" class="form-control mt-2" id="username" placeholder="Username">
+                        </div>
+
+                        <div class="form-group col-md-12">
+                            <label for="email">Email</label>
+                            <input type="email" class="form-control mt-2" id="email" placeholder="Email">
+                        </div>
+
+                        <!--div class="form-group col-md-12">
+                            <label for="phone">Phone</label>
+                            <input type="text" class="form-control mt-2" id="phone" placeholder="Phone Number">
+                        </div-->
+
+                        <div class="form-group col-md-12">
+                            <label for="password">Password</label>
+                            <input type="password" class="form-control mt-2" id="password" placeholder="Password">
+                        </div>
+
+                        <div class="form-group col-md-12">
+                            <label for="password">Confirm Password</label>
+                            <input type="password" class="form-control mt-2" id="password" placeholder="Password">
+                        </div>
+                    </form>
+                    <div class="form-group col-md-6 mx-auto">
+                        <button class="btn form-btn mt-0">Sign Up</button>
+                    </div>
+                </div>
+                <!--vendor-form-end-->
+                <div class="product-feed-tab pt-3" id="info-dd" style="height: 300px; overflow-y: scroll;">
+                    <form id="user-signup-form" class="row my-0">
+                        <div class="form-group col-md-12">
+                            <label for="full_name">Name</label>
+                            <input type="text" class="form-control mt-2" id="full_name" placeholder="Full Name">
+                        </div>
+
+                        <div class="form-group col-md-12">
+                            <label for="gender">Gender</label>
+                            <select id="gender" class="form-control mt-2">
+                                <option selected disabled>Gender</option>
+                                <option value="M">Male</option>
+                                <option value="F">Female</option>
+                            </select>
+                        </div>
+
+                        <div class="form-group col-md-12">
+                            <label for="user_email">Email</label>
+                            <input type="email" class="form-control mt-2" id="user_email" placeholder="Email">
+                        </div>
+
+                        <!--div class="form-group col-md-12">
+                            <label for="user_phone">Phone</label>
+                            <input type="text" class="form-control mt-2" id="user_phone" placeholder="Phone Number">
+                        </div-->
+
+                        <div class="form-group col-md-12">
+                            <label for="user_password">Password</label>
+                            <input type="password" class="form-control mt-2" id="user_password" placeholder="Password">
+                        </div>
+
+                        <div class="form-group col-md-12">
+                            <label for="conf_user_password">Confirm Password</label>
+                            <input type="password" class="form-control mt-2" id="conf_user_password"
+                                placeholder="Confirm Password">
+                        </div>
+                    </form>
+                    <div class="form-group col-md-6 mx-auto">
+                        <button class="btn form-btn mt-0">Sign Up</button>
+                    </div>
+                </div>
+                <!--user-form-tab end-->
+                <div class="form-group text-center">
+                    <p>Already have an account?<br><a href="#" id="login-lk" class="fud-link">Login</a></p>
+                </div>
+            </div>
+
+        </div>
+
+        <a href="" title=""><i class="la la-times-circle-o"></i></a>
+    </div>
 </div>
