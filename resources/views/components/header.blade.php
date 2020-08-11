6a26018788587ca860dc0780abbf1a28e9567218
Gabriel Osuobiem <osuobiem@gmail.com>
2020-08-12 00:46:19 +0100

    Modified header component to reflect new template

---
+++ resources/views/components/header.blade.php
@@ -0,0 +1,82 @@
+<!-- Header -->
+<div class="row m-0">
+	<div class="col-3 col-md-4 col-lg-3 p-0">
+		<nav class="navbar navbar-expand navbar-light bg-light osahan-nav-top p-1 h-80" id="lt-h">
+			<div class="container">
+				<a class="navbar-brand mr-2" href="index.html"><img id="logo" src="{{ url('assets/img/logo.svg') }}" alt="">
+				</a>
+			</div>
+		</nav>
+	</div>
+	<div class="col-6 col-md-4 col-lg-6 p-0">
+		<nav class="navbar navbar-expand navbar-light bg-light osahan-nav-top p-1 h-80 justify-content-center" id="mt-h">
+			<form class="form-inline mr-auto my-2 my-md-0 mw-100">
+				<div class="input-group">
+					<input type="text" class="form-control " placeholder="Search fudplug" aria-label="Search"
+						aria-describedby="basic-addon2">
+					<div class="input-group-append">
+						<button class="btn btn-primary hover-lift" type="button">
+							<i class="feather-search"></i>
+						</button>
+					</div>
+				</div>
+			</form>
+		</nav>
+	</div>
+	<div class="col-3 col-md-4 col-lg-3 p-0">
+		<nav class="navbar navbar-expand navbar-light bg-light osahan-nav-top h-80 p-1" id="rt-h">
+			<div class="container justify-content-center">
+				<ul class="navbar-nav ml-auto d-flex align-items-center">
+
+					<!-- Nav Item - User Information -->
+					@if(Auth::guest())
+					<li class="nav-item nav-link ml-1 pr-0">
+						<a class="btn btn-primary text-light hover-lift" href="#">
+							<strong>
+								<i class="feather-log-in d-none d-sm-inline"></i> Login
+							</strong>
+						</a>
+					</li>
+					@else
+					<li class="nav-item dropdown no-arrow ml-1 osahan-profile-dropdown pr-3 d-none d-lg-flex">
+						<a class="nav-link dropdown-toggle pr-0 h-link" href="#" role="button" data-toggle="dropdown"
+							aria-haspopup="true" aria-expanded="false">
+							<span class="noti-pin d-none"></span>
+							<i class="feather-bell feather-18 icon-hover bright-ic"></i>
+						</a>
+						<div class="dropdown-menu dropdown-menu-right shadow noti-drop">
+							<h6 class="dropdown-header text-center">
+								Notifications
+							</h6>
+							<div class="dropdown-divider"></div>
+							<div id="h-noti-cont" class="p-2 text-center">
+								<p>No Notifications yet!</p>
+							</div>
+						</div>
+					</li>
+
+					<li class="nav-item dropdown no-arrow ml-1 osahan-profile-dropdown">
+						<a class="nav-link dropdown-toggle pr-0 u-link" href="#" role="button" data-toggle="dropdown"
+							aria-haspopup="true" aria-expanded="false">
+							<p class="m-auto d-none d-md-block bright-ic" style="font-size: medium;">
+								John Doe
+							</p>
+							&nbsp; &nbsp;
+							<img class="img-profile rounded-circle" src="#">
+							<i class="feather-chevron-down"></i>
+						</a>
+						<!-- Dropdown - User Information -->
+						<div class="dropdown-menu dropdown-menu-right shadow">
+							<a class="dropdown-item" href="#"><i class="feather-user mr-1"></i> Profile</a>
+							<a class="dropdown-item" href="#"><i class="feather-settings mr-1"></i> Settings</a>
+							<a class="dropdown-item" href="#"><i class="feather-log-out mr-1"></i>
+								Logout</a>
+							<div class="dropdown-divider"></div>
+						</div>
+					</li>
+					@endif
+				</ul>
+			</div>
+		</nav>
+	</div>
+</div>
--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -1,58 +1,75 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
 
 <head>
-    <meta charset="UTF-8">
-    <title>fudplug</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="description" content="" />
-    <meta name="keywords" content="" />
-    <link rel="stylesheet" type="text/css" href="{{ url('assets/css/animate.css') }}">
-    <link rel="stylesheet" type="text/css" href="{{ url('assets/css/bootstrap.min.css') }}">
-    <link rel="stylesheet" type="text/css" href="{{ url('assets/css/line-awesome.css') }}">
-    <link rel="stylesheet" type="text/css" href="{{ url('assets/css/line-awesome-font-awesome.min.css') }}">
-    <link href="{{ url('assets/vendor/fontawesome-free/css/all.min.css') }}" rel="stylesheet" type="text/css">
-    <link rel="stylesheet" type="text/css" href="{{ url('assets/css/font-awesome.min.css') }}">
-    <link rel="stylesheet" type="text/css" href="{{ url('assets/css/jquery.mCustomScrollbar.min.css') }}">
-    <link rel="stylesheet" type="text/css" href="{{ url('assets/lib/slick/slick.css') }}">
-    <link rel="stylesheet" type="text/css" href="{{ url('assets/lib/slick/slick-theme.css') }}">
-    <link rel="stylesheet" type="text/css" href="{{ url('assets/css/style.css') }}">
-    <link rel="stylesheet" type="text/css" href="{{ url('assets/css/responsive.css') }}">
-    <link rel="stylesheet" type="text/css" href="{{ url('assets/css/custom.css') }}">
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <link rel="icon" type="image/png" href="{{ url('assets/img/fav.png') }}">
+    <title>@yield('title')fudplug</title>
+
+    <link rel="stylesheet" type="text/css" href="{{ url('assets/css/animate.min.css') }}" />
+    <!-- Feather Icon-->
+    <link href="{{ url('assets/vendor/icons/feather.css') }}" rel="stylesheet" type="text/css">
+    <!-- Bootstrap core CSS -->
+    <link href="{{ url('assets/vendor/bootstrap/css/bootstrap.min.css') }}" rel="stylesheet">
+
+    <link href="{{ url('assets/css/light.css') }}" rel="stylesheet">
+    <!-- Custom styles for this template -->
+    <link href="{{ url('assets/css/custom.css') }}" rel="stylesheet">
+
+    <script src="{{ url('assets/vendor/jquery/jquery.min.js') }}"></script>
+    <script src="{{ url('assets/vendor/jquery-cookie/jquery-cookie.js') }}"></script>
 </head>
 
 <body>
 
-    <div class="wrapper">
 
-        {{-- Menu Component --}}
-        @include('components.menu')
+    @include('components.header')
 
+    <div class="container-fluid">
+        <div class="row">
 
-        {{-- Main Component --}}
-        @yield('main')
+            {{-- Left Section --}}
+            <div class="col-lg-3 side-section px-1">
+                @section('left-section')
+                @show
+            </div>
 
-
-        {{-- Modals --}}
-        @include('components.modals')
-
-        {{-- Login Modal --}}
-        @include('components.login')
-
-        {{-- Sign Up Modal --}}
-        @include('components.signup')
-
+        </div>
     </div>
 
-    {{-- Scripts --}}
-    <script type="text/javascript" src="{{ url('assets/js/jquery.min.js') }}"></script>
-    <script type="text/javascript" src="{{ url('assets/js/popper.js') }}"></script>
-    <script type="text/javascript" src="{{ url('assets/js/bootstrap.min.js') }}"></script>
-    <script type="text/javascript" src="{{ url('assets/js/jquery.mCustomScrollbar.js') }}"></script>
-    <script type="text/javascript" src="{{ url('assets/lib/slick/slick.min.js') }}"></script>
-    <script type="text/javascript" src="{{ url('assets/js/scrollbar.js') }}"></script>
-    <script type="text/javascript" src="{{ url('assets/js/script.js') }}"></script>
-    <script type="text/javascript" src="{{ url('assets/js/custom.js') }}"></script>
+    {{-- @if(!Auth::guest())
+    @endif --}}
+    <!-- Mobile Bottom Menu -->
+    <div class="mobile-bottom d-lg-none">
+        <nav class="navbar-bottom navbar-expand navbar-light bg-light osahan-nav-top p-1 h-80" id="b-m">
+            <div class="container row m-0 text-center px-0">
+                <div class="col-3">
+                    <i class="feather-home feather-24 p-2 mbm-active"></i>
+                </div>
+                <div class="col-3">
+                    <i class="feather-bell feather-24 p-2"></i>
+                </div>
+                <div class="col-3">
+                    <i class="feather-list feather-24 p-2"></i>
+                </div>
+                <div class="col-3">
+                    <i class="feather-user feather-24 p-2"></i>
+                </div>
+            </div>
+        </nav>
+    </div>
+
+
+    <!-- Bootstrap core JavaScript -->
+    <script src="{{ url('assets/vendor/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+    <!-- slick Slider JS-->
+    <script type="text/javascript" src="{{ url('assets/vendor/slick/slick.min.js') }}"></script>
+    <!-- Custom scripts for all pages-->
+    <script src="{{ url('assets/js/osahan.js') }}"></script>
 </body>
 
-</html>
+</html
