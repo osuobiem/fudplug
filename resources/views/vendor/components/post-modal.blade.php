75b1eaf79c5428436412cef2a1b088e706e7acff
Gabriel Osuobiem <osuobiem@gmail.com>
2020-08-31 21:30:06 +0100

    Post Modal Media Container design done

--- resources/views/vendor/components/post-modal.blade.php
+++ resources/views/vendor/components/post-modal.blade.php
@@ -1,4 +1,4 @@
-{{-- info/Pop Modal --}}
+<!-- Post Modal Launcher -->
 <div class="post-modal-init">
   <div class="box shadow-sm border rounded bg-white mb-3 osahan-share-post">
     <ul class="nav nav-justified border-bottom osahan-line-tab" id="myTab" role="tablist">
@@ -10,10 +10,10 @@
     </ul>
     <div class="tab-content" id="myTabContent">
       <div class="tab-pane fade show active" id="home" role="tabpanel" aria-labelledby="home-tab">
-        <div class="p-4 d-flex align-items-center w-100" href="#">
-          <div class="w-100">
+        <div class="px-3 pt-2 d-flex align-items-center w-100" href="#">
+          <div class="post-textarea-cont-2 w-100">
             <textarea placeholder="Post something delicious..." class="form-control border-0 p-0 shadow-none"
-              rows="1"></textarea>
+              rows="2"></textarea>
           </div>
         </div>
       </div>
@@ -21,7 +21,7 @@
   </div>
 </div>
 
-{{-- Post Modal --}}
+<!-- Post Modal -->
 <div class="post-modal post-modal animate__animated animate__fadeIn d-none">
   <div class="box shadow-sm border rounded bg-white mb-3 osahan-share-post">
     <ul class="nav nav-justified border-bottom osahan-line-tab" id="myTab" role="tablist">
@@ -32,16 +32,28 @@
         </a>
       </li>
     </ul>
+
+    <!-- Post Text -->
     <div class="tab-content" id="myTabContent">
       <div class="tab-pane fade show active" id="home" role="tabpanel" aria-labelledby="home-tab">
-        <div class="p-4 d-flex align-items-center w-100" href="#">
-          <div class="w-100">
+        <div class="px-3 pt-2 d-flex align-items-center w-100" href="#">
+          <div class="post-textarea-cont w-100">
             <textarea placeholder="Post something delicious..." class="form-control border-0 p-0 shadow-none post-input"
               rows="5" id="post-textarea"></textarea>
           </div>
         </div>
       </div>
     </div>
+
+    <!-- Media Container -->
+    <div class="post-modal-media-container post-media-container">
+      <div class="pm pm-4 pmmc-i" style="background-image: url('{{ url('assets/img/test/1.jpg') }}')"></div>
+      <div class="pm pm-4 pmmc-i" style="background-image: url('{{ url('assets/img/test/2.jpg') }}')"></div>
+      <div class="pm pm-4 pmmc-i" style="background-image: url('{{ url('assets/img/test/3.jpg') }}')"></div>
+      <div class="pm pm-4 pmmc-i" style="background-image: url('{{ url('assets/img/test/4.jpg') }}')"></div>
+    </div>
+
+    <!-- Post Modal Foot -->
     <div class="border-top p-3 d-flex align-items-center">
       <div class="mr-auto">
         <a href="#" class="post-ico"><i class="la la-camera-retro la-2x p-1 icon-hover"></i></a>
