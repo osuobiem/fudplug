999a8b2a5857d7cabf140dc0b30e1997ffecf2dc
Gabriel Osuobiem <osuobiem@gmail.com>
2020-09-26 23:30:21 +0100

    Add feed preloader

--- resources/views/feed.blade.php
+++ resources/views/feed.blade.php
@@ -6,7 +6,15 @@
 @include('vendor.components.post-modal')
 @endif
 
-<div id="in-post-container"></div>
+<div id="in-post-container">
+  <div id="video-spinner"
+    class="justify-content-center text-center w-100 pb-2 box shadow-sm border rounded bg-white p-2">
+    <p><strong>Loading Feed</strong></p>
+    <div class="spinner-border spinner-border-sm btn-pr p-2" role="status">
+      <span class="sr-only">Loading...</span>
+    </div>
+  </div>
+</div>
 
 <script>
   $(document).ready(function () {
