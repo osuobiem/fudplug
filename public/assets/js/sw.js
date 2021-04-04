1232fb79b432970f8e348c07444aadb88e41024b
Gabriel Osuobiem <osuobiem@gmail.com>
2021-04-04 19:52:30 +0100

    Fix service worker bug

--- public/assets/js/sw.js
+++ public/assets/js/sw.js
@@ -4,7 +4,7 @@ icon = 'http://127.0.0.1:8000/assets/img/fav.png';
 self.addEventListener('push', function(event) {
     const body = event.data ? event.data.text() : false;
     
-    if(payload !== false) {
+    if(body !== false) {
       event.waitUntil(
         self.registration.showNotification('Fudplug', {body, icon})
       );
