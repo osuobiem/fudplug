51be3dcc797d634502a04cc829f37d0fce1c6d33
wisdomgratitudeindia <wisdom.gratitudeindia@gmail.com>
2021-04-05 03:19:10 +0100

    Vendor profile modified

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -285,9 +285,10 @@
                     @include('user.user-script')
                     <!-- USER SCRIPTS -->
                     @endif
-                    <!-- Add CSRF Token to Headers for Ajax Requests -->
-                    <script>
+                     <!-- Additional Scripts -->
+                     <script>
                         $(document).ready(function () {
+                            // Add CSRF Token to Headers for Ajax Requests
                             $.ajaxSetup({
                                 headers: {
                                     'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
@@ -296,7 +297,47 @@
                         });
 
                     </script>
-                    <!-- Add CSRF Token to Headers for Ajax Requests -->
+
+                    <!-- Handle alert for email activation -->
+                    @if(session()->has('verify_status'))
+                    @if(session()->get('verify_status') == true)
+                    <script>
+                        showAlert(true, "Your account is activated, you can log in now.");
+
+                    </script>
+                    @else
+                    <script>
+                        showAlert(false, "Invalid token.");
+
+                    </script>
+                    @endif
+                    @endif
+                    <!-- Handle alert for email activation -->
+
+                    <!-- Handle alert for socialite login -->
+                    @if(session()->has('soclogin_status'))
+                    @if(session()->get('soclogin_status') == true)
+                    <script>
+                        showAlert(true, "Logged in successfully.");
+
+                    </script>
+                    @else
+                    <script>
+                        showAlert(false, "This account doesn't exist.");
+
+                    </script>
+                    @endif
+                    @endif
+
+                    @if(session()->has('soclogin_error'))
+                    <script>
+                        showAlert(false, "{{session()->get('soclogin_error')}}");
+
+                    </script>
+                    @endif
+                    <!-- Handle alert for socialite login -->
+
+                    <!-- Additional Scripts -->
 </body>
 
 </html
